73e7db0298042c4f8970cca2f3c77a71568cc6bd
Handika Rafli A <shandika231@gmail.com>
2022-10-15 09:29:41 +0700

    to push

--- lib/res/assets_res.dart
+++ lib/res/assets_res.dart
@@ -3,4 +3,8 @@
 // ignore_for_file: constant_identifier_names
 // ignore_for_file: lines_longer_than_80_chars
 class AssetsRes {
+  static const String DRIVER = 'assets/driver.json';
+  static const String LAPTOP = 'assets/laptop.json';
+  static const String LOGIN = 'assets/login.json';
+  static const String REGISTER = 'assets/register.json';
 }
