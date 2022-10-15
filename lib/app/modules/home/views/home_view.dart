6b8227d0e6c1b7b0541449aa39d8dce346b3ab3b
Handika Rafli A <shandika231@gmail.com>
2022-10-15 10:42:24 +0700

    refactor home view

--- lib/app/modules/home/views/home_view.dart
+++ lib/app/modules/home/views/home_view.dart
@@ -50,45 +50,7 @@ class HomeView extends GetView<HomeController> {
         bottom: false,
         child: Column(
           children: [
-            Material(
-              elevation: 3,
-              child: Container(
-                padding: const EdgeInsets.symmetric(
-                  horizontal: 24,
-                  vertical: 20,
-                ),
-                child: Row(
-                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                  children: [
-                    const Text(
-                      'Chats',
-                      style: TextStyle(
-                        fontSize: 35,
-                        fontWeight: FontWeight.w600,
-                      ),
-                    ),
-                    Material(
-                      borderRadius: BorderRadius.circular(50),
-                      color: Colors.red[900],
-                      child: InkWell(
-                        onTap: () {
-                          Get.toNamed(Routes.PROFILE);
-                        },
-                        borderRadius: BorderRadius.circular(50),
-                        child: const Padding(
-                          padding: EdgeInsets.all(6.0),
-                          child: Icon(
-                            Icons.person,
-                            size: 35,
-                            color: Colors.white,
-                          ),
-                        ),
-                      ),
-                    ),
-                  ],
-                ),
-              ),
-            ),
+            const HeaderContent(),
             Expanded(
               child: ListView.builder(
                 itemCount: listOfChat.length,
@@ -97,6 +59,55 @@ class HomeView extends GetView<HomeController> {
                 },
               ),
             )
+          ],
+        ),
+      ),
+    );
+  }
+}
+
+class HeaderContent extends StatelessWidget {
+  const HeaderContent({
+    Key? key,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Material(
+      elevation: 3,
+      child: Container(
+        padding: const EdgeInsets.symmetric(
+          horizontal: 24,
+          vertical: 20,
+        ),
+        child: Row(
+          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+          children: [
+            const Text(
+              'Chats',
+              style: TextStyle(
+                fontSize: 35,
+                fontWeight: FontWeight.w600,
+              ),
+            ),
+            Material(
+              borderRadius: BorderRadius.circular(50),
+              color: Colors.red[900],
+              child: InkWell(
+                onTap: () {
+                  Get.toNamed(Routes.PROFILE);
+                },
+                borderRadius: BorderRadius.circular(50),
+                child: const Padding(
+                  padding: EdgeInsets.all(6.0),
+                  child: Icon(
+                    Icons.person,
+                    size: 35,
+                    color: Colors.white,
+                  ),
+                ),
+              ),
+            ),
           ],
         ),
       ),
