da77dc4e147ea5b843c2063e16581effe163aa29
Handika Rafli A <shandika231@gmail.com>
2022-10-16 07:30:32 +0700

    clear slicing change profile view

--- lib/app/application.dart
+++ lib/app/application.dart
@@ -44,7 +44,7 @@ class _ApplicationState extends State<Application> {
             title: "Chat App",
             debugShowCheckedModeBanner: false,
             initialRoute: splashController.isSplash.value
-                ? Routes.UPDATE_STATUS
+                ? Routes.CHANGE_PROFILE
                 : AppPages.INTRODUCTION,
             getPages: AppPages.routes,
           );
--- lib/app/modules/change_profile/views/change_profile_view.dart
+++ lib/app/modules/change_profile/views/change_profile_view.dart
@@ -1,5 +1,5 @@
+import 'package:avatar_glow/avatar_glow.dart';
 import 'package:flutter/material.dart';
-
 import 'package:get/get.dart';
 
 import '../controllers/change_profile_controller.dart';
@@ -10,13 +10,112 @@ class ChangeProfileView extends GetView<ChangeProfileController> {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: const Text('ChangeProfileView'),
+        backgroundColor: Colors.red[900],
+        leading: IconButton(
+          onPressed: () {
+            Get.back();
+          },
+          icon: const Icon(Icons.chevron_left),
+        ),
+        actions: const [Icon(Icons.save)],
+        title: const Text('Change Profile'),
         centerTitle: true,
       ),
-      body: Center(
-        child: Text(
-          'ChangeProfileView is working',
-          style: TextStyle(fontSize: 20),
+      body: Padding(
+        padding: const EdgeInsets.all(24),
+        child: ListView(
+          children: [
+            AvatarGlow(
+              endRadius: 80,
+              duration: const Duration(seconds: 2),
+              glowColor: Colors.grey,
+              repeat: true,
+              child: Container(
+                width: 120,
+                height: 120,
+                decoration: BoxDecoration(
+                  borderRadius: BorderRadius.circular(60),
+                  image: const DecorationImage(
+                    image: AssetImage('assets/profile.png'),
+                    fit: BoxFit.contain,
+                  ),
+                ),
+              ),
+            ),
+            const SizedBox(
+              height: 12,
+            ),
+            const CustomTextField(hint: 'Email'),
+            const SizedBox(
+              height: 12,
+            ),
+            const CustomTextField(hint: 'Name'),
+            const SizedBox(
+              height: 12,
+            ),
+            const CustomTextField(hint: 'Status'),
+            const SizedBox(
+              height: 12,
+            ),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: [
+                const Text(
+                  'No Image',
+                ),
+                TextButton(
+                  onPressed: () {},
+                  child: const Text(
+                    "Choose Image",
+                  ),
+                ),
+              ],
+            ),
+            const SizedBox(
+              height: 20,
+            ),
+            ElevatedButton(
+              style: ElevatedButton.styleFrom(
+                backgroundColor: Colors.red[900],
+                shape: RoundedRectangleBorder(
+                  borderRadius: BorderRadius.circular(10),
+                ),
+              ),
+              child: const Text('UPDATE'),
+              onPressed: () {},
+            )
+          ],
+        ),
+      ),
+    );
+  }
+}
+
+class CustomTextField extends StatelessWidget {
+  final String hint;
+  const CustomTextField({
+    Key? key,
+    required this.hint,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return TextField(
+      cursorColor: Colors.red[900],
+      decoration: InputDecoration(
+        hintText: hint,
+        focusedBorder: OutlineInputBorder(
+          borderRadius: BorderRadius.circular(24),
+          borderSide: BorderSide(
+            color: Colors.red[900]!,
+          ),
+        ),
+        border: OutlineInputBorder(
+          borderSide: BorderSide(
+            color: Colors.red[900]!,
+            width: 1,
+          ),
+          borderRadius: BorderRadius.circular(24),
         ),
       ),
     );
